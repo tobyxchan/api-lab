7478b2e5bd3438fb3fe380859d7dd15e8302fcf2
Jim Clark <jim.clark@generalassemb.ly>
2022-10-12 07:21:57 -0700

    Add boilerplate

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const username = req.query.username;
+  console.log(`username: ${username}`);
+  res.render('index');
 });
 
 module.exports = router;
