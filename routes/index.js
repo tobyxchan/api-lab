dd6ebe5345185dbc54ca1d35b60cec3795f3b0da
Jim Clark <jim.clark@generalassemb.ly>
2022-10-12 08:16:33 -0700

    Finish

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+// Uncomment if you don't have fetch in node
+// const fetch = require('node-fetch');
+
+const token = process.env.GITHUB_TOKEN;
+const ROOT_URL = 'https://api.github.com';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   const username = req.query.username;
-  console.log(`username: ${username}`);
-  res.render('index');
+  if (!username) return res.render('index', { userData: null });
+  const options = {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  };
+  const userData = await fetch(`${ROOT_URL}/users/${username}`, options)
+    .then(res => res.json());
+  userData.repos = await fetch(userData.repos_url, options)
+    .then(res => res.json());
+  res.render('index', { userData });
+  // let userData;
+  // fetch(`${ROOT_URL}/users/${username}`, options)
+  //   .then(res => res.json())
+  //   .then(userInfo => {
+  //     userData = userInfo;
+  //     return fetch(userData.repos_url, options);
+  //   })
+  //   .then(res => res.json())
+  //   .then(repos => {
+  //     console.log(repos[0]);
+  //     res.render('index', { userData });
+  //   })
 });
 
 module.exports = router;
